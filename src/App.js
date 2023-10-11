fbf376f766ee1af8ea8c691db0079872f53c5602
Pvamsi02 <cvlnspvamsi2003d@gmail.com>
2023-10-11 20:01:45 +0530

    updated logic

--- src/App.js
+++ src/App.js
@@ -2,36 +2,21 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [state, setState] = useState({
-    wordCount: 0,
-  });
+  const [state, setState] = useState("");
+  const wordCount = state.split(/\s+/).filter(Boolean).length;
   const handleInput = (e) => {
-    let string = e.target.value;
-    let c = 0;
-    let string1 = string.split(" ");
-    for (let index = 0; index < string1.length; index++) {
-      if (string.length === 0) {
-        setState({
-          wordCount: 0,
-        });
-      }
-      if (string1[index] !== " " && string1[index].length > 0) {
-        c++;
-      }
-      setState({ wordCount: c });
-    }
+    setState(e.target.value);
   };
   return (
     <div className="App">
       <h4>Responsive Paragraph Word Counter</h4>
       <textarea
-        rows="10"
-        cols="50"
         placeholder="Text Input"
         onChange={handleInput}
+        value={state}
       ></textarea>
       <p className="wc">
-        Word Count: <span className="num">{state.wordCount}</span>{" "}
+        Word Count: <span className="num">{wordCount}</span>{" "}
       </p>
       <p>Chillara V L N S Pavana Vamsi</p>
     </div>
